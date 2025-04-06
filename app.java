953dd396cc6fc9d55585bd09acd7c06cd936c0bd
thanhtsuna <thanhnatsu2006@gmail.com>
2025-04-06 18:18:38 +0700

    Add files via upload

---
+++ app.java
@@ -0,0 +1,15 @@
+import javax.swing.*;
+import java.awt.*;
+
+public class app extends JFrame {
+    public app(){
+        setTitle("app");
+        setSize(600,600);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLayout(new BorderLayout());
+        setLocationRelativeTo(null);
+    }
+    public static void main(String[] args) {
+        
+    }
+}
